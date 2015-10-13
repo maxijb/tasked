b0f4e28c0a38b36729340ba81442d06dbf3adc44
Maximiliano Benedetto <infoprisma1@gmail.com>
2015-10-13 13:01:14 +0200

    refactor d&d

--- assets/js/modules/dragng/dragngContainer.js
+++ assets/js/modules/dragng/dragngContainer.js
@@ -9,63 +9,44 @@ export default function() {
 
           let timeout           = null,
               timeoutToStart    = 200,
+              dragId            = attrs.dragngId || "",
               $doc              = $(document),
-              $placeholder      = $('<div data-dragng="placeholder" class="dragng-placeholder" style="background:red; height: 50px;"></div>'),
+              $placeholder      = $(`<div data-dragng="placeholder" class="dragng-placeholder" style="background:red; height: 50px;" data-dragng-id="${dragId}" ></div>`),
               placeholderOnBody = false,
               $body             = $('body'),
-              dragId            = attrs.dragngId,
               dragIdCondition   = !!dragId ? `[data-dragng-id=${dragId}]`: "",
               horizontal        = attrs.hasOwnProperty('horizontal'),
-              pointerEvents     = 'pointerEvents' in document.documentElement.style,
-              handlerWidth      = 0,
-              handlerHeight     = 0,
+              handler           = { width: 0, height: 0 },
               start,
-              $copy;
+              $copy,
+              pointerEvents     = (function(){
+                                      var element = document.createElement('x'),
+                                          documentElement = document.documentElement,
+                                          getComputedStyle = window.getComputedStyle,
+                                          supports;
+                                      if(!('pointerEvents' in element.style)){
+                                          return false;
+                                      }
+                                      element.style.pointerEvents = 'auto';
+                                      element.style.pointerEvents = 'x';
+                                      documentElement.appendChild(element);
+                                      supports = getComputedStyle && 
+                                          getComputedStyle(element, '').pointerEvents === 'auto';
+                                      documentElement.removeChild(element);
+                                      return !!supports;
+                                  })();
           
+
+
+
           //set default object to avoid errors on empty callbacks
           if (!scope.callbacks) scope.callbacks = {};
           
-          //add the id to the placeholder
-          $placeholder.attr('data-dragng-id', dragId);
           /////////////////////////////////////////////////
           // TODO how to fix this
           if (horizontal) $placeholder.css("float", "left");
           ///////////////////////////////////////////////////
           ////////////////////////////////////////////////
-
-          /* function called to kill the timeout previous to a drag and drop */
-          function cancelTimeout(isDragging) {
-            clearTimeout(timeout);
-            $doc.off('mouseup.dragngTimeout')
-                .off('mouseleave.dragngTimeout');
-              if (!isDragging) {
-                $('body').removeClass('noselect');
-              }
-          }
-
-
-          /* Positions the handler according to wheter the pointerEvents are enabled */
-          $.fn.dragNgPositionHandler = function(e) {
-             let top  = pointerEvents ? e.pageY - handlerHeight / 2 : e.pageY + 15,
-                 left = pointerEvents ? e.pageX - handlerWidth / 2 : e.pageX + 15;
-              return $(this).css({top, left});
-          }
-
-          /* Finishes drag session */
-          function endDrag() {
-                //call function to finish drag
-                typeof scope.callbacks.endDrag === "function" && scope.callbacks.endDrag(start);
-                $doc.off('mousemove.dragng')
-                    .off('mouseleave.dragng')
-                    .off('mouseup.dragng')
-                    .find('body')
-                    .removeClass('noselect');
-
-
-                //remove copied element
-                $placeholder.remove();
-                $copy.remove();
-          }
 
 
           //delegate event to start drag
@@ -115,13 +96,13 @@ export default function() {
              $copy = $orig.clone()
                       .addClass('drag-ng-copy')
                       .width($orig.width())
-                      .height($orig.height())
-                      .dragNgPositionHandler(e);
+                      .height($orig.height());
+
               
               //cache handler size into variables
-              handlerHeight = $copy.outerHeight();
-              handlerWidth = $copy.outerWidth();
-
+              handler.height = $copy.outerHeight();
+              handler.width = $copy.outerWidth();
+              positionHandler($copy, e, handler);
 
               //call callback if availble
               typeof scope.callbacks.drag === "function" && scope.callbacks.drag(start);
@@ -180,7 +161,7 @@ export default function() {
              .on('mousemove.dragng', (e) => {
 
                 //move the copy with the mouse              
-                $copy.dragNgPositionHandler(e);
+                positionHandler($copy, e, handler);
                 
                 //find the closest element drag related
                 let $ref = $(e.target).closest('[data-dragng]'+dragIdCondition),
@@ -247,6 +228,43 @@ export default function() {
 
           };
           
+
+
+          ///////////////////// FUNCTIONS ////////////////
+
+          /* function called to kill the timeout previous to a drag and drop */
+          function cancelTimeout(isDragging) {
+            clearTimeout(timeout);
+            $doc.off('mouseup.dragngTimeout')
+                .off('mouseleave.dragngTimeout');
+              if (!isDragging) {
+                $('body').removeClass('noselect');
+              }
+          }
+
+
+          /* Positions the handler according to wheter the pointerEvents are enabled */
+          function positionHandler($copy, e, handlerData) {
+             let top  = pointerEvents ? e.pageY - handlerData.height / 2 : e.pageY + 15,
+                 left = pointerEvents ? e.pageX - handlerData.width / 2 : e.pageX + 15;
+              return $copy.css({top, left});
+          }
+
+          /* Finishes drag session */
+          function endDrag() {
+                //call function to finish drag
+                typeof scope.callbacks.endDrag === "function" && scope.callbacks.endDrag(start);
+                $doc.off('mousemove.dragng')
+                    .off('mouseleave.dragng')
+                    .off('mouseup.dragng')
+                    .find('body')
+                    .removeClass('noselect');
+
+
+                //remove copied element
+                $placeholder.remove();
+                $copy.remove();
+          }
 
 
       }
